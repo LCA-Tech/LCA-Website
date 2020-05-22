a550120326b2c4bacd4cd9884f47236696af5dc8
Shumbul Arifa <shumbul.181co152@nitk.edu.in>
2020-05-22 12:24:21 +0530

    Add files via upload

--- lca_website/lca_website/urls.py
+++ lca_website/lca_website/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
@@ -21,4 +22,5 @@ from ngo import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r"^", include("ngo.urls", namespace="ngo")),
+    path('', views.homepage, name="ngo"),
 ]
