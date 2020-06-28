277e832b7cb0693a7704e022c58a9a385a336d5b
Adharsh Kamath <adharshkamathr@gmail.com>
2020-06-29 00:44:42 +0530

    NGO display added

--- lca_website/ngo/urls.py
+++ lca_website/ngo/urls.py
@@ -4,5 +4,7 @@ from ngo import views
 app_name = "ngo"
 
 urlpatterns = [
+    path('', views.NgoListView.as_view(), name='list'),
     path('signup', views.signup, name='signup'),
+    path('<int:pk>', views.NgoDetailView.as_view(), name='ngo'),
 ]
